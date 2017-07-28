112c97c5e56636dfc88d5527044581bbaa7e94fa
Carlos Anguiano <c.joseanguiano@gmail.com>
2017-07-28 08:51:45 -0500

    Java program that uses char array, loop

---
+++ src/com/devix/www/Main.java
@@ -0,0 +1,16 @@
+package com.devix.www;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char[] values = new char[4];
+        values[0] = 'j';
+        values[1] = 'a';
+        values[2] = 'v';
+        values[3] = 'a';
+
+        for (char val: values) {
+            System.out.println(val);
+        }
+    }
+}
